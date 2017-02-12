95620e0460eaa1f05d45f2532f47083cfdada30a
pigram86 <todd@toddpigram.com>
2017-02-11 19:39:30 -0500

    Cookbook Apache

---
+++ cookbooks/apache/recipes/websites.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: apache
+# Recipe:: websites
+#
+# Copyright:: 2017, Todd Pigram, All Rights Reserved.
+
+
+file 'default www' do
+  path '/var/www/html/index.html'
+  content 'Hellow World!'
+  owner 'root'
+  group 'root'
+  mode 00755
+  action :create
+end
